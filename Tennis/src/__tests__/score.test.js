93184002ccc64207a50e1f2ce62e3952383894b6
Mickaël Saada <mickael.saada@fluo.com>
2018-06-07 23:50:13 +0200

    refactor(tennis): remove redondant tests

--- Tennis/src/__tests__/score.test.js
+++ Tennis/src/__tests__/score.test.js
@@ -2,6 +2,10 @@
 import { computeFinalScore } from "../score";
 
 describe("score", () => {
+  // RULE 2
+  /*
+    The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described as “love”, “fifteen”, “thirty”, and “forty” respectively.
+  */
   it("should return love-love when no player score", () => {
     //given
     const playerScores = [];
@@ -9,21 +13,20 @@ describe("score", () => {
     //then
     expect(score).toEqual("love-love");
   });
-  it("should return love-fifteen when player 2 score one point", () => {
+  it("should return fifteen-fifteen when each player scores one point", () => {
     //given
-    const playerScores = [2];
-    const score = computeFinalScore(playerScores);
-    //then
-    expect(score).toEqual("love-fifteen");
-  });
-
-  it("should return love-thirty when player 2 scores two points", () => {
-    //given
-    const playerScores = [2, 2];
+    const playerScores = [1, 2];
     //when
     const score = computeFinalScore(playerScores);
     //then
-    expect(score).toEqual("love-thirty");
+    expect(score).toEqual("fifteen-fifteen");
+  });
+  it("should return thirty-thirty when each player scores two points", () => {
+    //given
+    const playerScores = [1, 1, 2, 2];
+    const score = computeFinalScore(playerScores);
+    //then
+    expect(score).toEqual("thirty-thirty");
   });
 
   it("should return love-forty when player 2 scores three points", () => {
@@ -34,51 +37,12 @@ describe("score", () => {
     //then
     expect(score).toEqual("love-forty");
   });
+  // END RULE 2
 
-  it("should return fifteen-fifteen when each player scores one point", () => {
-    //given
-    const playerScores = [1, 2];
-    //when
-    const score = computeFinalScore(playerScores);
-    //then
-    expect(score).toEqual("fifteen-fifteen");
-  });
-
-  it("should return deuce when player 1 and 2 score 3 points each", () => {
-    //given
-    const playerScores = [1, 1, 1, 2, 2, 2];
-    //when
-    const score = computeFinalScore(playerScores);
-    //then
-    expect(score).toEqual("deuce");
-  });
-  it("should return deuce when player 1 and 2 score at least 3 points and the scores are equal", () => {
-    //given
-    const playerScores = [1, 1, 1, 2, 2, 2, 1, 2];
-    //when
-    const score = computeFinalScore(playerScores);
-    //then
-    expect(score).toEqual("deuce");
-  });
-
-  it("should return 'player1 won' when there is no deuce during game", () => {
-    //given
-    const playerScores = [1, 1, 1, 1];
-    //when
-    const score = computeFinalScore(playerScores);
-    //then
-    expect(score).toEqual("player1 won");
-  });
-
-  it("should return 'player2 won' when there is no deuce during game", () => {
-    //given
-    const playerScores = [2, 2, 2, 2];
-    //when
-    const score = computeFinalScore(playerScores);
-    //then
-    expect(score).toEqual("player2 won");
-  });
-
+  // RULE 4
+  /*
+    If at least three points have been scored by each side and a player has one more point than his opponent, the score of the game is “advantage” for the player in the lead.
+  */
   it("should return 'advantage player1' when player 1 scores after a deuce", () => {
     //given
     const playerScores = [1, 1, 1, 2, 2, 2, 1];
@@ -103,7 +67,6 @@ describe("score", () => {
     //then
     expect(score).toEqual("advantage player2");
   });
-
   it("should return 'advantage player1' when player 1 scores after multiple deuces", () => {
     //given
     const playerScores = [1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1];
@@ -112,6 +75,34 @@ describe("score", () => {
     //then
     expect(score).toEqual("advantage player1");
   });
+  // END RULE 4
+
+  // RULE 3
+  /*
+    If at least three points have been scored by each player, and the scores are equal, the score is “deuce”.
+  */
+  it("should return 'deuce' when player 1 and 2 score 3 points each", () => {
+    //given
+    const playerScores = [1, 1, 1, 2, 2, 2];
+    //when
+    const score = computeFinalScore(playerScores);
+    //then
+    expect(score).toEqual("deuce");
+  });
+  it("should return 'deuce' when player 1 and 2 score at least 3 points and the scores are equal", () => {
+    //given
+    const playerScores = [1, 1, 1, 2, 2, 2, 1, 2];
+    //when
+    const score = computeFinalScore(playerScores);
+    //then
+    expect(score).toEqual("deuce");
+  });
+  // END RULE 3
+
+  // RULE 1
+  /*
+    A game is won by the first player to have won at least four points in total and at least two points more than the opponent.
+  */
   it("should return 'player1 won' when the player1 scores at least 4 points and at least 2 points more than player 2", () => {
     //given
     const playerScores = [1, 1, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1];
@@ -128,4 +119,5 @@ describe("score", () => {
     //then
     expect(score).toEqual("player2 won");
   });
+  // END RULE 1
 });
