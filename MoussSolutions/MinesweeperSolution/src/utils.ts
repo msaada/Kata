e185ee3a626fef67c1d13d046c3838aa627d8776
Mickaël Saada <mickael.saada@fluo.com>
2018-06-15 13:58:11 +0200

    chore(kata): remove mouss solutions

--- MoussSolutions/MinesweeperSolution/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { concat, reduce } from 'lodash/fp';
-
-type Flatten<T> = (arg: ReadonlyArray<ReadonlyArray<T>>) => ReadonlyArray<T>;
-export const flatten = <T>(
-  arg: ReadonlyArray<ReadonlyArray<T>>
-): ReadonlyArray<T> =>
-  reduce((acc: ReadonlyArray<T>, row: ReadonlyArray<T>) => [...acc, ...row])(
-    []
-  )(arg);
